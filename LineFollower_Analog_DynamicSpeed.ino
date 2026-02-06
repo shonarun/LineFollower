29e198a5dd1b9150a70c69bd848e713260dee464
shonarun <150151101+shonarun@users.noreply.github.com>
2026-02-06 17:28:53 +0530

    Update LineFollower_Analog_DynamicSpeed.ino

--- LineFollower_Analog_DynamicSpeed.ino
+++ LineFollower_Analog_DynamicSpeed.ino
@@ -69,7 +69,7 @@ void stopMotors() {
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
